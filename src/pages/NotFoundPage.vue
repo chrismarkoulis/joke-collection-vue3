f5b5b29e542681c5eab1775eba61d3c5f873e54d
Christos Markoulis <chrismarkoulis89@live.com>
2025-01-26 00:46:11 +0200

    updated vue-router

---
+++ src/pages/NotFoundPage.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="flex flex-col items-center justify-center text-center">
+    <h1 class="text-5xl font-bold text-red-500">404</h1>
+    <p class="text-xl text-gray-700 mt-2">The page does not exist.</p>
+    <router-link to="/" class="mt-6 px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600">
+      Go Back Home
+    </router-link>
+  </div>
+</template>
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,10 +1,12 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import CollectionPage from "../pages/CollectionPage.vue";
+import NotFoundPage from "../pages/NotFoundPage.vue";
 
 const routes = [
   { path: "/", name: "Home", component: HomePage },
   { path: "/collection", name: "Collection", component: CollectionPage },
+  { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundPage },
 ];
 
 const router = createRouter({
