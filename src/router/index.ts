3907d6e95c3c6012a6b09200a571eecc1c8a99a5
Christos Markoulis <chrismarkoulis89@live.com>
2025-01-26 13:08:29 +0200

    added vue-router types

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,15 +1,15 @@
-import { createRouter, createWebHistory } from "vue-router";
+import { createRouter, createWebHistory, type RouteRecordRaw, type Router } from "vue-router";
 import HomePage from "../pages/HomePage.vue";
 import CollectionPage from "../pages/CollectionPage.vue";
 import NotFoundPage from "../pages/NotFoundPage.vue";
 
-const routes = [
+const routes: RouteRecordRaw[] = [
   { path: "/", name: "Home", component: HomePage },
   { path: "/collection", name: "Collection", component: CollectionPage },
   { path: "/:pathMatch(.*)*", name: "NotFound", component: NotFoundPage },
 ];
 
-const router = createRouter({
+const router: Router = createRouter({
   history: createWebHistory(),
   routes,
 });
